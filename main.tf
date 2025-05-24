7e64650c0c9d9a55f9397b3f40d939a9c984c469
Andis <64140698+andisabdullah2@users.noreply.github.com>
2025-05-24 22:37:41 +0800

    first

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "CI-CD-Instance"
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region      = "ap-southeast-1"
+aws_access_key  = ""
+aws_secret_key  = ""
+ami_id          = "ami-0c02fb55956c7d316"  # Amazon Linux 2
+instance_type   = "t2.micro"
